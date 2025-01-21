9b6e7e22dd831a96c526e2823e5987e78062f142
Shinigami <funteshka5@gmail.com>
2025-01-21 19:59:53 +0500

    tomcat

---
+++ src/main/java/controllers/HelloController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+        return "hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "goodbye";
+    }
+}
